3b484c5b83e3e6bfcd04c6288d85cd7c0c409e7d
John Chia <john@nokturnal.org>
2016-09-13 14:54:37 -0700

    Including build settings

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,5 +7,6 @@ Java_com_johnchia_hotplate_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
     return env->NewStringUTF(hello.c_str());
 }
